ba631e612aa42df5a1fcc4e8fc5bce7c9ff0c983
wlgus <wlgus@DESKTOP-ETT4EV4>
2022-02-26 15:48:03 +0900

    codeup풀이

---
+++ Algorithm/src/algorithm_study/codeup/Problem_1014.java
@@ -0,0 +1,17 @@
+package algorithm_study.codeup;
+
+import java.util.Scanner;
+
+public class Problem_1014 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a,b;
+		String x,y;	
+		a = sc.next();
+		b = sc.next();	
+		x=b;
+		y=a;	
+		System.out.println(x +" " + y);
+	}
+
+}
---
+++ Algorithm/src/algorithm_study/codeup/Problem_1016.java
@@ -0,0 +1,13 @@
+package algorithm_study.codeup;
+
+import java.util.Scanner;
+
+public class Problem_1016 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in); //nextLine()은 분리자도 다 읽어올 수 있고, next(), nextInt()는 분리자는 제외하고 읽어온다
+		String x = sc.nextLine();
+		System.out.println(x);
+	}
+
+}
