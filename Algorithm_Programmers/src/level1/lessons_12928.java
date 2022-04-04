36cf6aea4180bd6699a552ef50698ab378aab10a
baejihyeon <wlgus6343@naver.com>
2022-04-05 00:08:45 +0900

    [Solve] ✍ 약수의 합 (level1)

--- Algorithm_Programmers/src/level1/lessons_12928.java
+++ Algorithm_Programmers/src/level1/lessons_12928.java
@@ -6,7 +6,6 @@ public class lessons_12928 {
         for(int i=1; i<n+1; i++) //  0으로 나눌 수 가 없으니 +1을 꼭 해줘야함 
         {
             if(n%i == 0)
-            
                 answer += i; 
         }
         return answer;
